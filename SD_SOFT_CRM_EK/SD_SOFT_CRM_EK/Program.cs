edcf2a77a44076465ed5ce59aea1fb8271c9d81c
bajar <bajarmilan2001@gmail.com>
2022-08-29 16:17:02 +0200

    EK_Diagram

---
+++ SD_SOFT_CRM_EK/SD_SOFT_CRM_EK/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SD_SOFT_CRM_EK
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
