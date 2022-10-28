58ea20edeb356da8752103a539d1c89818fb4470
bajar <bajarmilan2001@gmail.com>
2022-10-28 02:05:42 +0200

    revert

---
+++ CRM.Domain/Services/PasswordHash/IPasswordHasherService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Domain.Services.PasswordHasher
+{
+    public interface IPasswordHasherService
+    {
+        String PasswordEncodeing(string password);
+        String PasswordDecodeing(string password);
+    }
+}
